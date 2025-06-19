979031f7886452a5bdd4ebb68228692a55e97f88
hunterMotko <huntermotko@gmail.com>
2025-06-18 21:08:16 -0400

    check failure on lint

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,23 +1,22 @@
 import { describe, expect, test } from "vitest";
 import { IncomingHttpHeaders } from "http";
-import { config } from "src/config";
 import { getAPIKey } from "src/api/auth";
 
 const headers: IncomingHttpHeaders = {
-  authorization: "ApiKey " + "KEYTESTINGGJNKLJKLSDAJFLKSJDFKL",
+	authorization: "ApiKey " + "KEYTESTINGGJNKLJKLSDAJFLKSJDFKL",
 };
 
 const notHeaders: IncomingHttpHeaders = {
-  authorization: "Bearer " + "KEYTESTINGGJNKLJKLSDAJFLKSJDFKL",
+	authorization: "Bearer " + "KEYTESTINGGJNKLJKLSDAJFLKSJDFKL",
 };
 
 describe("TEST GETAPIKEY", () => {
-  test("api should be equal", () => {
-    expect(getAPIKey(headers)).toBe("KEYTESTINGGJNKLJKLSDAJFLKSJDFKL");
-  });
+	test("api should be equal", () => {
+		expect(getAPIKey(headers)).toBe("KEYTESTINGGJNKLJKLSDAJFLKSJDFKL");
+	});
 
-  test("should be incorrect header", () => {
-    expect(getAPIKey(notHeaders)).not.toBe("KEYTESTINGGJNKLJKLSDAJFLKSJDFKL");
-    expect(getAPIKey(notHeaders)).toBe(null);
-  });
+	test("should be incorrect header", () => {
+		expect(getAPIKey(notHeaders)).not.toBe("KEYTESTINGGJNKLJKLSDAJFLKSJDFKL");
+		expect(getAPIKey(notHeaders)).toBe(null);
+	});
 });
