abe2536d378aafae2033abe4531dbda1d537dfd9
hunterMotko <huntermotko@gmail.com>
2025-06-18 20:21:35 -0400

    tests and ci updates

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "vitest";
+import { IncomingHttpHeaders } from "http";
+import { config } from "src/config";
+import { getAPIKey } from "src/api/auth";
+
+const headers: IncomingHttpHeaders = {
+	authorization: "ApiKey " + "KEYTESTINGGJNKLJKLSDAJFLKSJDFKL"
+}
+
+const notHeaders: IncomingHttpHeaders = {
+	authorization: "Bearer " + "KEYTESTINGGJNKLJKLSDAJFLKSJDFKL"
+}
+
+describe("TEST GETAPIKEY", () => {
+	test("api should be equal", () => {
+		expect(getAPIKey(headers)).toBe("KEYTESTINGGJNKLJKLSDAJFLKSJDFKL");
+	});
+
+	test("should be incorrect header", () => {
+		expect(getAPIKey(notHeaders)).not.toBe("KEYTESTINGGJNKLJKLSDAJFLKSJDFKL");
+		expect(getAPIKey(notHeaders)).toBe(null);
+	});
+});
